e40837f6cee16cf86f1b16545616ac50d00b311f
Leandro Atallah <leandroatallah@gmail.com>
2024-02-04 18:13:59 -0300

    chore: initial config of drop animation

--- src/modules/cards/components/Card/index.tsx
+++ src/modules/cards/components/Card/index.tsx
@@ -1,7 +1,14 @@
-import { Draggable } from "@hello-pangea/dnd";
+import {
+  Draggable,
+  DraggableStateSnapshot,
+  DraggingStyle,
+  DropAnimation,
+  NotDraggingStyle,
+} from "@hello-pangea/dnd";
 
 import { CardItem } from "@/modules/cards/models";
 import * as Composition from "./CardComposition";
+import { CSSProperties } from "react";
 
 interface CardProps {
   data?: CardItem;
@@ -22,30 +29,58 @@ export const Card = ({
     return <Composition.Root card={null} isTurned className={className} />;
   }
 
+  const getStyle = (
+    style: DraggingStyle | NotDraggingStyle | undefined,
+    snapshot: DraggableStateSnapshot
+  ): CSSProperties => {
+    const dropping: DropAnimation | undefined | null = snapshot.dropAnimation;
+    if (!dropping) {
+      return style || {};
+    }
+    const { moveTo, curve, duration } = dropping;
+    const translate = `translate(${moveTo.x}px, ${moveTo.y}px)`;
+    const rotate = "rotate(1turn)";
+    return {
+      ...style,
+      transform: `${translate} ${rotate}`,
+      // slowing down the drop
+      transition: `all ${curve} ${duration + 1}s`,
+    };
+  };
+
   return (
     <Draggable
       draggableId={data.deckId}
       index={index}
       isDragDisabled={disabled}
     >
-      {(provided) => (
-        <div
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          <Composition.Root
-            card={data}
-            isTurned={isTurned}
-            className={className}
+      {(provided, snapshot) => {
+        const isDragging = snapshot.isDragging && !snapshot.isDropAnimating;
+        return (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            className={
+              isDragging
+                ? "!outline-2 !outline-red-600 outline-double outline-offset-1"
+                : ""
+            }
+            // style={getStyle(provided.draggableProps.style, snapshot)}
           >
-            {disabled && "disabled"}
-            <Composition.Header />
-            <Composition.Title />
-            {data.isChampion && <Composition.FooterChampion />}
-          </Composition.Root>
-        </div>
-      )}
+            <Composition.Root
+              card={data}
+              isTurned={isTurned}
+              className={className}
+            >
+              {disabled && "disabled"}
+              <Composition.Header />
+              <Composition.Title />
+              {data.isChampion && <Composition.FooterChampion />}
+            </Composition.Root>
+          </div>
+        );
+      }}
     </Draggable>
   );
 };
